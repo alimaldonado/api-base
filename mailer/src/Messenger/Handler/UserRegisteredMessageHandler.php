a4136fdec2e4a91ffc93cfb7f44d609481dc3011
ali.maldonado <alimaldonado.1995@gmail.com>
2020-11-14 13:03:49 -0700

    added register email message

---
+++ mailer/src/Messenger/Handler/UserRegisteredMessageHandler.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Mailer\Messenger\Handler;
+
+use Mailer\Messenger\Message\UserRegisteredMessage;
+use Mailer\Service\Mailer\ClientRoute;
+use Mailer\Service\Mailer\MailerService;
+use Mailer\Templating\TwigTemplate;
+use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
+
+class UserRegisteredMessageHandler implements MessageHandlerInterface
+{
+    protected MailerService $mailerService;
+
+    protected string $host;
+
+    public function __construct(MailerService $mailerService, string $host)
+    {
+        # code...
+        $this->mailerService = $mailerService;
+        $this->host = $host;
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function __invoke(UserRegisteredMessage $message): void
+    {
+        $payload = [
+            'name' => $message->getName(),
+            'url' => \sprintf(
+                '%s%s?token=%s&uid=%s',
+                $this->host,
+                ClientRoute::ACTIVATE_ACCOUNT,
+                $message->getToken(),
+                $message->getId()
+            ),
+        ];
+        
+        $this->mailerService->send($message->getEmail(), TwigTemplate::USER_REGISTER, $payload);
+
+        
+    }
+}
--- mailer/src/Serializer/Messenger/EventSerializer.php
+++ mailer/src/Serializer/Messenger/EventSerializer.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Mailer\Serializer\Messenger;
 
 use Mailer\Messenger\Message\UserRegisteredMessage;
@@ -8,19 +10,23 @@ use Symfony\Component\Messenger\Transport\Serialization\Serializer;
 
 class EventSerializer extends Serializer
 {
-    public function decode(array $encodedEnvelop): Envelope 
+    public function decode(array $encodedEnvelop): Envelope
     {
-        $translatedType =$this->translateType($encodedEnvelop['headers']['type']);
+        $translatedType = $this->translateType($encodedEnvelop['headers']['type']);
+
         $encodedEnvelop['headers']['type'] = $translatedType;
+
         return parent::decode($encodedEnvelop);
     }
+
     private function translateType(string $type): string
     {
         $map = ['App\Messenger\Message\UserRegisteredMessage' => UserRegisteredMessage::class];
 
-        if(\array_key_exists($type, $map)){
+        if (\array_key_exists($type, $map)) {
             return $map[$type];
         }
+
         return $type;
     }
 }
