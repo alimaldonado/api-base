dc7cf245245e4390d4f782f6eb4c2e7c1d285cbd
ali.maldonado <alimaldonado.1995@gmail.com>
2020-11-14 09:50:08 -0700

    configured mailer

---
+++ mailer/src/Messenger/Message/RoutingKey.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Mailer\Messenger\Message;
+
+class RoutingKey
+{
+    public const USER_QUEUE = 'user_queue';
+
+    
+}
---
+++ mailer/src/Messenger/Message/UserRegisteredMessage.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Mailer\Messenger\Message;
+
+class UserRegisteredMessage
+{
+    private string $id;
+
+    private string $name;
+
+    private string $email;
+
+    private string $token;
+
+    public function __construct(string $id, string $name, string $email, string $token)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->email = $email;
+        $this->token = $token;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function getToken()
+    {
+        return $this->token;
+    }
+}
